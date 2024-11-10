03ddfd02a1a9fa5ca4adc09c859cfc100b8f4945
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2024-11-10 10:29:18 +0530

    Added angular material

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,14 +4,26 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {NaviComponent} from './navi/navi.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ webapp/src/app/navi/navi.component.ts
@@ -0,0 +1,19 @@
+import {Component, inject} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+
+@Component({
+    selector: 'app-navi',
+    templateUrl: './navi.component.html',
+    styleUrl: './navi.component.css'
+})
+export class NaviComponent {
+    private breakpointObserver = inject(BreakpointObserver);
+
+    isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+        .pipe(
+            map(result => result.matches),
+            shareReplay()
+        );
+}
