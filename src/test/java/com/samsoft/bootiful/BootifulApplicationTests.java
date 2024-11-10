765d91ae185c573cd84055b7e0909b9c835db6fd
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2024-11-10 09:07:37 +0530

    Hello World project

---
+++ src/test/java/com/samsoft/bootiful/BootifulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsoft.bootiful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
